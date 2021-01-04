32af1765518e62e4a9a0d6263bbdd497b9c11946
zjw256576 <18345301605@163.com>
2021-01-04 17:06:27 +0800

    2021-01-04 17.06

--- app/src/main/java/com/yunxuan/huirongzhipei/IndexActivity.java
+++ app/src/main/java/com/yunxuan/huirongzhipei/IndexActivity.java
@@ -3,10 +3,16 @@ package com.yunxuan.huirongzhipei;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class IndexActivity extends AppCompatActivity {
-
+    private ImageView iv_message;
+    private LinearLayout ll_tonggao;
+    private ImageView iv_match;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +21,32 @@ public class IndexActivity extends AppCompatActivity {
         if(actionBar!=null){
             actionBar.hide();
         }
+        initView();
+    }
+
+    private void initView() {
+        iv_message=findViewById(R.id.iv_message);
+        ll_tonggao=findViewById(R.id.ll_tonggao);
+        iv_match=findViewById(R.id.iv_match);
+        iv_message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(IndexActivity.this,MessageActivity.class);
+                startActivity(intent);
+            }
+        });
+        ll_tonggao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(IndexActivity.this,SystemNoticeActivity.class);
+                startActivity(intent);
+            }
+        });
+        iv_match.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/yunxuan/huirongzhipei/SystemNoticeActivity.java
+++ app/src/main/java/com/yunxuan/huirongzhipei/SystemNoticeActivity.java
@@ -7,18 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MessageActivity extends AppCompatActivity {
-    private ImageView iv_back_index;
+public class SystemNoticeActivity extends AppCompatActivity {
+    private ImageView iv_sys_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message);
+        setContentView(R.layout.activity_system_notice);
         ActionBar actionBar = getSupportActionBar();
         if(actionBar!=null){
             actionBar.hide();
         }
-        iv_back_index=findViewById(R.id.iv_back_index);
-        iv_back_index.setOnClickListener(new View.OnClickListener() {
+        initView();
+    }
+
+    private void initView() {
+        iv_sys_back=findViewById(R.id.iv_sys_back);
+        iv_sys_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
