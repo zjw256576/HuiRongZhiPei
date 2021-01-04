a33a93a50b49ff263f214b7f56830e2b6af624f0
zjw256576 <18345301605@163.com>
2021-01-04 13:42:49 +0800

    2021-01-04

--- app/src/main/java/com/yunxuan/huirongzhipei/IndexActivity.java
+++ app/src/main/java/com/yunxuan/huirongzhipei/IndexActivity.java
@@ -1,5 +1,6 @@
 package com.yunxuan.huirongzhipei;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,9 @@ public class IndexActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.hide();
+        }
     }
 }
